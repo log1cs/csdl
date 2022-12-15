c3de5615f47cf4c5fe20e6bfff111404334c62bb
log1cs <tuan73176@gmail.com>
2022-12-15 13:47:20 +0700

    15122022 Update [2/2]

--- chenxaukytu.c
+++ chenxaukytu.c
@@ -1,47 +1,22 @@
 #include<stdio.h>
-#include<conio.h>
 #include<string.h>
 
-int main () {
-	int p, q, i, j, k, slength, wlength;
-	char w[100], s[100], r[100], x[100];
-	printf("Nhap W = ");
-	gets(w);
-	printf("Nhap S = ");
-	gets(s);
-	printf("P = ");
-	scanf("%d", &p);
-	printf("Q = ");
-	scanf("%d", &q);
-
-	// Debugging purposes only - Print the input strings
-	//printf("%s", w);
-	//printf("\n%s\n", s);
-
-	slength = strlen(s);
-	wlength = strlen(w);
-
-	if (p > slength && s > wlength) {
-		exit(0);
-	} else {
-		for (i = 0; i < p; i++) {
-		r[i] = w[i];
-		}
-
-		k = i;
-
-		for (j = 0; s[j] !='\0';j++) {
-		r[k] = s[j];
-		k = k + 1;
-		}
-
-		for (i; w[i] != '\0'; i++) {
-		r[k] = w[i];
-		k = k + 1;
-		}
-	}
-
-	r[k] = '\0';
-	printf("Ket qua: %s", r);
-	return 0;
+int main(){
+    char s[1000], w[1000];
+    int i = 0;
+    printf("Nhap W = "); gets(w);
+    printf("Nhap S = "); gets(s);
+    int p, q;
+    printf("P = "); scanf("%d", &p);
+    printf("Q = "); scanf("%d", &q);
+    if (p > strlen(w) || q > strlen(w) || p < 0 || q < 0) return 0;
+    else{
+        printf("Ket qua: ");
+        for (i = 0; i < strlen(w); i++){
+            if((i == p || i == q) && p != q) printf("%s", s);
+            else if (i == p && i == q) printf("%s%s", s, s);
+            printf("%c", w[i]);
+        }
+    }
+    return 0;
 }
--- taochuoi3.c
+++ taochuoi3.c
@@ -1,26 +1,30 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<math.h>
 
 int main () {
 	char w[100];
 	int s;
-        size_t i, j;
+	size_t i, j;
 	printf("Nhap W = ");
 	gets(w);
 
-	printf("Cac xau con la:\n");
-
-	size_t n = strlen(w);
-	size_t pos = 0;
-	char temp[4];
-	temp[3] = '\0';
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < 3; j++) {
-			temp[j] = w[pos];
-			pos = (pos + 1) % n;
+	if (strlen(w) == 0 || strlen(w) == 1) {
+		printf("Cac xau con la:");
+	} else {
+		printf("Cac xau con la:\n");
+		size_t n = strlen(w);
+		size_t pos = 0;
+		char temp[4];
+		temp[3] = '\0';
+		for (i = 0; i < n; i++) {
+			for (j = 0; j < 3; j++) {
+				temp[j] = w[pos];
+				pos = (pos + 1) % n;
+			}
+		printf("%s\n", temp);
 		}
-	printf("%s\n", temp);
 	}
 	return 0;
 }
