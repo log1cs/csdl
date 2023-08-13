a9ace1a7435ef36719b0f86acfc046399bf516af
log1cs <tuan73176@gmail.com>
2023-08-13 08:52:53 +0700

    13082023: Add more function to conv

--- csdl/conv.cpp
+++ csdl/conv.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
-#include <cstring>
-#include <cmath>
-#include <algorithm>
+#include<iostream>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+#include<algorithm>
 
 template<typename T>
 class Stack
@@ -84,6 +85,69 @@ class Stack
     int TopOfStack;    
 };    
 
+// Ham kiem tra so duoc nhap vao co phai
+// la so thap phan hay khong.
+bool IsInt(double d)
+{
+     return d == (int) d;
+}
+
+// Ham chuyen so nguyen/so thap phan thanh xau ky tu
+std::string fconv(double a)
+{
+     if (IsInt(a) == true) {
+          std::cout << "Ket qua: " << a << std::endl;
+     } else {
+          // Setup conversion
+          std::ostringstream stream1;
+          stream1 << std::fixed;
+          stream1 << std::setprecision(1);
+          stream1 << a;
+          std::string string1 = stream1.str();
+          
+          int locale;
+          std::cout << "Output la 1 so thap phan, chon 1 format: " << std::endl;
+          do
+          {
+               std::cout << "1. en_US (dau cham)" << std::endl;
+               std::cout << "2. fr_FR (dau phay)" << std::endl;
+               std::cout << "3. In output o ca 2 format" << std::endl;
+               std::cout << "Chon (1 - 3): ";
+               std::cin >> locale;
+
+               if (locale == 1)
+               {
+                    std::cout << "Da chon " << locale << std::endl;
+                    std::replace(string1.begin(), string1.end(), ',', '.');
+                    std::cout << "Ket qua: " << string1 << std::endl;
+               } else if (locale == 2) {
+                    std::cout << "Da chon " << locale << std::endl;
+                    std::replace(string1.begin(), string1.end(), '.', ',');
+                    std::cout << "Ket qua: " << string1 << std::endl;
+               } else if (locale == 3) {
+                    std::cout << "Da chon " << locale << std::endl;
+
+                    // Setup conversion
+                    std::ostringstream stream2;
+                    stream2 << std::fixed;
+                    stream2 << std::setprecision(1);
+                    stream2 << a;
+                    std::string string2 = stream2.str();
+
+                    std::replace(string1.begin(), string1.end(), ',', '.');
+                    std::cout << "Ket qua (dau cham): " << string1 <<  std::endl;
+                    std::replace(string2.begin(), string2.end(), '.', ',');
+                    std::cout << "Ket qua (dau phay): " << string2 << std::endl;
+               } else {
+                    std::cout << "Khong hop le. Hay nhap lai!" << std::endl;
+               }
+          } while (locale < 1 || locale > 3);
+     }
+
+     // Placebo de khu warning
+     return "1";
+}
+
 // Ham kiem tra ky tu trong mang bt[] co phai 1 toan tu hay khong.
 bool check(char bt[]) 
 {
@@ -112,15 +176,16 @@ int Priority(char pnx)
 }
 
 /* Chuyen bieu thuc tu trung to sang hau to. */
-std::string conv(std::string str, bool check)
+std::string conv(std::string str, bool replace)
 {
      Stack<char> sdm;
 
-     if (check == true) {
+     if (replace == true) {
           std::replace(str.begin(), str.end(), ',', '.');
           std::replace(str.begin(), str.end(), 'x', '*');
      }
 
+     // Khoi tao string q trong.
      std::string q = "";
      int i = 0;
      while (i < str.length())
@@ -180,6 +245,7 @@ double calc(char bt[])
                msm.pop();
                double b = msm.top();
                msm.pop();
+               
                if (strcmp(q, "+") == 0) {
                     c = b + a;
                } else if (strcmp(q, "-") == 0) {
@@ -191,6 +257,7 @@ double calc(char bt[])
                } else if (strcmp(q, "^") == 0) {
                     c = pow(b, a);
                }
+               
                msm.push(c);
           } else {
                msm.push(atof(q));
@@ -202,11 +269,11 @@ double calc(char bt[])
 
 int main()
 {
-     // Lam sach VS terminal truoc khi chay.
+     // Lam sach cua so terminal truoc khi chay.
      system("clear");
      char z[300];
      std::string inp;
-     std::cout << "Infix: ";
+     std::cout << "Nhap bieu thuc trung to: ";
      getline(std::cin, inp);
      std::string v = conv(inp, true);
 
@@ -214,7 +281,7 @@ int main()
      p.resize(p.size() - 1);
 
      std::cout << std::endl;
-     std::cout << "Post-fix conversion: " << p << std::endl;
+     std::cout << "Bieu thuc hau to: " << p << std::endl;
      for (int i = 0; i < v.length(); i++)
      {
           if (z[i] == ',')
@@ -225,6 +292,6 @@ int main()
           z[i] = v[i];
      }
      std::cout << std::endl;
-     std::cout << "Result: " << calc(z) << std::endl;
+     fconv(calc(z));
      return 0;
 }
